8406f4b0fff5946445a8df1fc36899187e4976d1
abdou-chakhkhar <abdelmoula.chakhkhar@uit.ac.ma>
2021-12-10 16:52:07 +0100

    first commit latest

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,25 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth'
+
+
+const config = {
+    apiKey: "AIzaSyCMSnKEJREyHG6mPLOcgyz7WR-f3qzAkm8",
+    authDomain: "ecommerceproject-68566.firebaseapp.com",
+    projectId: "ecommerceproject-68566",
+    storageBucket: "ecommerceproject-68566.appspot.com",
+    messagingSenderId: "938543521745",
+    appId: "1:938543521745:web:d209b761c835581e708af0",
+    measurementId: "${config.measurementId}"
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters( { prompt: 'select_account'} )
+export const signInWithGoogle = () => auth.signInWithGoogle(provider);
+
+export default firebase;
